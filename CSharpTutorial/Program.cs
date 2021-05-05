c89e4e42b488a4cdcd3f710550df71c54d981f29
Timothy Hart <timothyhart6@gmail.com>
2021-05-05 08:12:49 -0400

    First Commit

---
+++ CSharpTutorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CSharpTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var file = File.ReadAllText(@"/Users/timothyhart/Desktop/hello_world.jpg");
+            Console.WriteLine(file);
+
+            foreach (var line in file)
+            {
+                //Console.WriteLine(line);
+            }
+        }
+    }
+    
+}
